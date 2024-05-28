6da254be6e56310b5e9f2d9ebfdad33a22b2e249
marcosfavoretti <marcosfavojr@gmail.com>
2024-05-28 10:11:01 -0300

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsCadComponent } from './shared/widgets/forms-cad/forms-cad.component';
 import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
@@ -36,17 +34,14 @@ import { ButtonCanvaComponent } from './shared/widgets/button-canva/button-canva
     ButtonCanvaComponent,
   ],
   imports: [
-    DialogModule,
-    InputTextModule,
     ButtonModule,
     BadgeModule,
+    InputTextModule,
+    DialogModule,
     PanelMenuModule,
-    BrowserAnimationsModule,
-    ToastModule,
     CardModule,
-    FormsModule,
-    BrowserModule,
-    NgbModule
+    ToastModule,
+
   ],
   providers: [MessageService],
   bootstrap: [AppComponent]
